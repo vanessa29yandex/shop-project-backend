64274f4c5c95495a3f9e812fe0f8527ba862ebe0
Irina Tolshinsky <vanessa29@yandex.ru>
2023-12-10 19:48:12 +0200

    ,

--- controllers/payments.controller.js
+++ controllers/payments.controller.js
@@ -18,6 +18,8 @@ const createPayment = async (req, res, next) => {
   const expiration_month = expDate.slice(0, 2);
   const expiration_year = expDate.slice(-2);
 
+  const last_digits = creditNumber.slice(-4);
+
   const {
     user_address: { city, street, building, apartment },
   } = userDetails;
@@ -28,7 +30,7 @@ const createPayment = async (req, res, next) => {
     const order = await Order.create({
       user,
       customer_details,
-      payment_details:{
+      payment_details: {
         transaction_number: '1234567890' + Date.now(),
         terminal_number: '1234567890',
         last_digits,
@@ -36,86 +38,44 @@ const createPayment = async (req, res, next) => {
       products,
     });
 
-  const requestBody = {
-    amount: cartTotal,
-    currency: "USD",
-    payment_method: {
-      type: "il_visa_card",
-      fields: {
-        number: creditNumber || "4111111111111111",
-        expiration_month,
-        expiration_year,
-        name: userDetails.user_name,
-        cvv,
-        address,
+    const requestBody = {
+      amount: cartTotal,
+      currency: 'USD',
+      payment_method: {
+        type: 'il_visa_card',
+        fields: {
+          number: creditNumber,
+          expiration_month,
+          expiration_year,
+          name: userDetails.user_name,
+          cvv,
+          address,
+        },
+        metadata: {
+          merchant_defined: true,
+        },
       },
-      metadata: {
-        merchant_defined: true,
-      },
-    },
-    error_payment_url: `${process.env.API_URL}/rejected-payment`,
-    complete_payment_url: `${process.env.API_URL}/success-payment?tpken=${order._id}`,
-    capture: true,
-  };
+      error_payment_url: `${process.env.CLIENT_URL}/rejected-payment`,
+      complete_payment_url: `${process.env.CLIENT_URL}/success-payment?token=${order._id}`,
+      capture: true,
+    };
 
-    const response = await makeRequest("POST", "/v1/payments", requestBody);
-
-    // console.log(response)
+    const response = await makeRequest('POST', '/v1/payments', requestBody);
 
     const { redirect_url: redirectUrl } = response.body.data;
 
     res.send({
       paymentStatus: {
         redirectUrl,
-        token:order._id,
+        token: order._id,
       },
     });
-  } catch (error) {
 
-  }
-
-    const transaction_number = paymentId.split("_")[1];
-    console.log(transaction_number)
-    const last_digits = creditNumber.slice(-4);
-
-    const order = await Order.create({
-      user: userDetails._id,
-      customer_details,
-      payment_details: {
-        transaction_number,
-        terminal_number: "1234567890",
-        transaction_date: response.body.data.created_at,
-        last_digits,
-      },
-      products,
-    });
-
-    res.send({
-      paymentStatus: {
-        redirectUrl,
-        paymentId,
-      },
-    });
   } catch (error) {
     console.log(error);
     next(error);
   }
 
-  // const terminal_number = '1234567890';
-  // const transaction_date = Date.now()
-  // const transaction_number = terminal_number + transaction_date;
-  // const last_digits = credit_number.slice(-4);
-
-  // res.status(200).send({
-  //   success: true,
-  //   message: 'Payment Successful',
-  //   paymentStatus: {
-  //     last_digits,
-  //     terminal_number,
-  //     transaction_number,
-  //     transaction_date
-  //   },
-  // });
 };
 
 module.exports = { createPayment };
